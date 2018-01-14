fc4706dbed2812b952e2687070692752d6d56c62
Mujahid <mkhan9047@gmail.com>
2018-01-14 20:56:25 +0600

    UI added

---
+++ app/src/main/java/com/example/mujahid/resultpublish/SaveMarks.java
@@ -0,0 +1,23 @@
+package com.example.mujahid.resultpublish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SaveMarks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_marks);
+    }
+
+    public void onEdit(View view) {
+
+    }
+
+
+    public void onSave(View view){
+
+    }
+}
