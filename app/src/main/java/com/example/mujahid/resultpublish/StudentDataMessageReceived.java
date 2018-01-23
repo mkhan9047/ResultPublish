5cf271c0825220c448b140de572e5e10c95509fa
Mujahid <mkhan9047@gmail.com>
2018-01-23 12:20:36 +0600

    communication between java class and activity success

---
+++ app/src/main/java/com/example/mujahid/resultpublish/StudentDataMessageReceived.java
@@ -0,0 +1,11 @@
+package com.example.mujahid.resultpublish;
+
+import java.util.HashMap;
+
+/**
+ * Created by Mujahid on 1/22/2018.
+ */
+
+public interface StudentDataMessageReceived {
+    public void onMessageReceived(Student student);
+}
